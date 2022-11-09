a7d773a5e8de6dc0c514d9feb28b424c635ae3a2
duduarena <caduarena@gmail.com>
2022-11-09 15:28:03 -0300

    perfil de desenvolvimento

--- src/main/java/com/valdir/helpdesk/services/DBService.java
+++ src/main/java/com/valdir/helpdesk/services/DBService.java
@@ -19,26 +19,25 @@ import com.valdir.helpdesk.repositories.TecnicoRepository;
 public class DBService {
 	@Autowired
 	private TecnicoRepository tecnicoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private ChamadoRepository chamadoRepository;
-	
 
 	public void instanciaDB() {
 		Tecnico tec1 = new Tecnico(null, "valdir cesar", "08106470016", "valdir@mail.com", "123");
 		tec1.addPerfil(Perfil.ADMIN);
 
-		Cliente cli1= new Cliente(null, "Linus Torvalds", "10036150070", "torvalds@mail.com", "123");
-			
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"chamado 01", "primeiro chamado", tec1, cli1);
-			
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "10036150070", "torvalds@mail.com", "123");
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "chamado 01", "primeiro chamado", tec1,
+				cli1);
 
 		tecnicoRepository.saveAll(Arrays.asList(tec1));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
-	
+
 }
