3ed66554644ab94f73d407d81fdf87f2029d5f74
duduarena <caduarena@gmail.com>
2022-11-11 14:10:23 -0300

    exceção para cpf e email ja cadastrados

--- src/main/java/com/valdir/helpdesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.valdir.helpdesk.domain.Pessoa;
 import com.valdir.helpdesk.domain.Tecnico;
 import com.valdir.helpdesk.domain.dtos.TecnicoDTO;
+import com.valdir.helpdesk.repositories.PessoaRepository;
 import com.valdir.helpdesk.repositories.TecnicoRepository;
+import com.valdir.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.valdir.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
@@ -16,34 +19,40 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
-	
-	
-	public Tecnico findById(Integer id ) {
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		
-		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado:" + id));
+
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado:" + id));
 	}
-
-
-
-
 
 	public List<Tecnico> findAll() {
 		// TODO Auto-generated method stub
 		return repository.findAll();
 	}
 
-
-
-
-
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
-Tecnico newObj = new Tecnico(objDTO);
-return repository.save(newObj);
+		validaPorCpfEEmail(objDTO);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
+
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		{
+			if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+				throw new DataIntegrityViolationException("CPF ja cadastrado");
+
+			}
+		}
+	obj = pessoaRepository.findByEmail(objDTO.getEmail());
+	if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+		throw new DataIntegrityViolationException("Email ja cadastrado");
 	
 	}
 
-
-	
-}
+	}}
